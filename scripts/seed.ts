fd24793debdc6963af1fd25e8b63bf8441af3f94
Script Raccoon <scriptraccoon@gmail.com>
2025-03-25 14:16:19 +0100

    fix seed script

---
+++ scripts/seed.ts
@@ -0,0 +1,28 @@
+import { createClient } from '@libsql/client'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const TURSO_DATABASE_URL = process.env.TURSO_DATABASE_URL ?? ''
+const TURSO_AUTH_TOKEN = process.env.TURSO_AUTH_TOKEN ?? ''
+
+const turso = createClient({
+	url: TURSO_DATABASE_URL,
+	authToken: TURSO_AUTH_TOKEN,
+})
+
+const sql = `
+CREATE TABLE
+    IF NOT EXISTS leaderboard (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        score INTEGER NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+`
+
+async function seed() {
+	await turso.execute(sql)
+	console.info('seeding done')
+}
+
+seed()
