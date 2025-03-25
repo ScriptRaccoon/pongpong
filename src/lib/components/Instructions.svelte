2fc650842569deaebcc756065f75fe75b7aef086
Script Raccoon <scriptraccoon@gmail.com>
2025-03-26 00:34:59 +0100

    improve instructions

---
+++ src/lib/components/Instructions.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition'
+
+	let show_instructions = $state(true)
+
+	function close_instructions() {
+		show_instructions = false
+	}
+</script>
+
+{#if show_instructions}
+	<div transition:slide={{ duration: 150 }}>
+		<p>
+			<strong>Instructions.</strong>
+			Control the two paddles with the keys
+			<kbd>Up</kbd> / <kbd>Down</kbd>. Each ball hit gives you a point. Watch out
+			for deviators (blue) and accelerators (red)!
+		</p>
+		<button onclick={close_instructions}>Ok</button>
+	</div>
+{/if}
+
+<style>
+	div {
+		display: grid;
+		align-items: center;
+		grid-template-columns: 1fr auto;
+		gap: 1rem;
+		margin-bottom: 1rem;
+	}
+
+	p {
+		color: var(--secondary-text-color);
+	}
+
+	kbd {
+		background-color: var(--card-color);
+		padding: 0.15rem 0.4rem;
+		color: var(--text-color);
+		border-radius: 0.2rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
 	import App from '$lib/components/App.svelte'
+	import Instructions from '$lib/components/Instructions.svelte'
 	import { CANVAS_HEIGHT, CANVAS_WIDTH } from '$lib/shared/config.js'
 
 	let canvas = $state<HTMLCanvasElement | null>(null)
 	let ctx = $derived(canvas?.getContext('2d') ?? null)
 </script>
 
-<p>
-	Control the two paddles with the keys
-	<kbd>Up</kbd> / <kbd>Down</kbd>.
-	<br />
-	Watch out for deviators and accelerators!
-</p>
+<Instructions />
 
 <canvas width={CANVAS_WIDTH} height={CANVAS_HEIGHT} bind:this={canvas}></canvas>
 
@@ -20,11 +16,6 @@
 {/if}
 
 <style>
-	p {
-		color: var(--secondary-text-color);
-		margin-bottom: 1rem;
-	}
-
 	canvas {
 		background-color: var(--black-color);
 		outline: 2px solid var(--card-color);
